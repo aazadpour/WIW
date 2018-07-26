f41cc4d93eec9af4c17bb3dcddbeff117c14bdc3
aazadpour <azetaworks@gmail.com>
2018-07-25 19:59:54 -0500

    fixed homepage/sched POM along with login page POM

---
+++ eclipse-workspace/AzetaProject/src/main/java/pageObject/WhenIWork/HomePageAndSchedulePom.java
@@ -0,0 +1,45 @@
+package pageObject.WhenIWork;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HomePageAndSchedulePom {
+
+	private static WebElement element = null;
+	private static String cssSelector = null;
+
+	// avatar homepage hover button
+	public static WebElement avatarHomepageHover_btn(WebDriver driver) {
+		element = driver.findElement(By.cssSelector("[class='avatar has-menu menu-icon']"));
+		return element;
+	}
+
+	public static String avatarHomepageHover_btn() {
+		cssSelector = "[class='avatar has-menu menu-icon']";
+		return cssSelector;
+	}
+
+	// my schedule button in avatar dropdown
+	public static WebElement avatarMySchedule_btn(WebDriver driver) {
+		element = driver.findElement(By.cssSelector("[href='/myschedule/']"));
+		return element;
+	}
+	
+	public static String avatarMySchedule_btn() {
+		cssSelector = "[href='/myschedule/']";
+		return cssSelector;
+	}
+	
+	// my schedule calendar table
+	public static WebElement myScheduleCalendar_tbl(WebDriver driver) {
+		element = driver.findElement(By.cssSelector("[class='calendar-table']"));
+		return element;
+	}
+	
+	public static String myScheduleCalendar_tbl() {
+		cssSelector = "[class='calendar-table']";
+		return cssSelector;
+	}
+}
+
--- eclipse-workspace/AzetaProject/src/main/java/pageObject/WhenIWork/LoginPagePom.java
+++ eclipse-workspace/AzetaProject/src/main/java/pageObject/WhenIWork/LoginPagePom.java
@@ -4,42 +4,44 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-
 public class LoginPagePom {
 
 	private static WebElement element = null;
 	private static String id = null;
 	private static String xpath = null;
 	private static String cssSelector = null;
-	
-	// signinUsername txtbx								
-		public static WebElement dashboardNavLink_btn(WebDriver driver){								
-			element = driver.findElement(By.id("dashboard-nav-link"));							
-			return element;							
-		}								
-		public static String dashboardNavLink_btn(){								
-			id = "dashboard-nav-link";							
-			return id;							
-		}
-	
-		// schedule title							
-		public static WebElement dashboardGreetingTitle_txt(WebDriver driver){								
-			element = driver.findElement(By.xpath("//*[@id=\"today-schedule\"]/div[1]/div"));							
-			return element;							
-		}								
-		public static String dashboardGreetingTitle_txt(){								
-			xpath = "//*[@id=\"today-schedule\"]/div[1]/div";							
-			return xpath;							
-		}
-		
-		// schedule title							
-		public static WebElement errorLoginRed_popup(WebDriver driver){								
-			element = driver.findElement(By.cssSelector("[class='notice alert alert-danger']"));							
-			return element;							
-		}								
-		public static String errorLoginRed_popup(){								
-			cssSelector = "[class='notice alert alert-danger']";							
-			return cssSelector;							
-		}
-		
+
+	// signinUsername txtbx
+	public static WebElement dashboardNavLink_btn(WebDriver driver) {
+		element = driver.findElement(By.id("dashboard-nav-link"));
+		return element;
+	}
+
+	public static String dashboardNavLink_btn() {
+		id = "dashboard-nav-link";
+		return id;
+	}
+
+	// schedule title
+	public static WebElement dashboardGreetingTitle_txt(WebDriver driver) {
+		element = driver.findElement(By.xpath("//*[@id=\"today-schedule\"]/div[1]/div"));
+		return element;
+	}
+
+	public static String dashboardGreetingTitle_txt() {
+		xpath = "//*[@id=\"today-schedule\"]/div[1]/div";
+		return xpath;
+	}
+
+	// error red popup
+	public static WebElement errorLoginRed_popup(WebDriver driver) {
+		element = driver.findElement(By.cssSelector("[class='notice alert alert-danger']"));
+		return element;
+	}
+
+	public static String errorLoginRed_popup() {
+		cssSelector = "[class='notice alert alert-danger']";
+		return cssSelector;
+	}
+
 }
